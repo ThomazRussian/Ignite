7fca0a9ae30fde7ff5df2668817724cc9f662ce9
Thomaz Russian <Thomazrussian@gmail.com>
2025-05-16 09:34:06 -0300

    Modulo 1 Commit

---
+++ FirstProject/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,11 @@
+import {Post} from './Post';
+export function App() {
+  return (
+    <div>
+
+      <Post />
+
+    </div>
+  )
+}
+
---
+++ FirstProject/01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,10 @@
+export function Post() {
+    return (
+        <p>Post</p>
+    )
+}
+
+// export default Post
+
+
+//Default export vs Named Exports
